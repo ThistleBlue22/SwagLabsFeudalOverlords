aa97c4d1f8df555a8f26ca31389dd440effa8b3e
andymurray32 <andrewmurray5@outlook.com>
2022-03-03 10:25:53 +0000

    added class running with only

--- src/test/java/com/feudaloverlords/swaglabs/runners/test.java
+++ src/test/java/com/feudaloverlords/swaglabs/runners/test.java
@@ -1,9 +1,10 @@
 package com.feudaloverlords.swaglabs.runners;
 
+import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-//@RunWith( Class running with)
+@RunWith(Cucumber.class)
 @CucumberOptions(
     //features={"  insert features path   "}
         //features={"pretty","html:target/testReport.html","json:target/jsonReport.json"}
