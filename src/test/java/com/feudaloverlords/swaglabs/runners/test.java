87997bc9bc79284f9fc4bc7a99e9379e2ed8b35b
andymurray32 <andrewmurray5@outlook.com>
2022-03-03 11:52:17 +0000

    Notes to discuss with group

--- src/test/java/com/feudaloverlords/swaglabs/runners/test.java
+++ src/test/java/com/feudaloverlords/swaglabs/runners/test.java
@@ -5,10 +5,29 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
+//@CucumberOptions(dryRun=true) check if all feature file steps have corresponding step definitions
+//@CucumberOptions(monochrome=true) console output from Cucumber in a readable format,
 @CucumberOptions(
     //features={"  insert features path   "}
-        //features={"pretty","html:target/testReport.html","json:target/jsonReport.json"}
+        //plugin={"pretty","html:target/testReport.html","json:target/jsonReport.json"}
+        //tags=" add tag, preferably epics",
         //publish= true;
         )
+
+/*
+cucumber.ansi-colors.disabled=  # true or false. default: false
+cucumber.execution.dry-run=     # true or false. default: false
+cucumber.execution.limit=       # number of scenarios to execute (CLI only).
+cucumber.execution.order=       # lexical, reverse, random or random:[seed] (CLI only). default: lexical
+cucumber.execution.strict=      # true or false. default: true.
+cucumber.execution.wip=         # true or false. default: false.
+cucumber.features=              # comma separated paths to feature files. example: path/to/example.feature, path/to/other.feature
+cucumber.filter.name=           # regex. example: .*Hello.*
+cucumber.filter.tags=           # tag expression. example: @smoke and not @slow
+cucumber.glue=                  # comma separated package names. example: com.example.glue
+cucumber.plugin=                # comma separated plugin strings. example: pretty, json:path/to/report.json
+cucumber.object-factory=        # object factory class name. example: com.example.MyObjectFactory
+cucumber.snippet-type=          # underscore or camelcase. default: underscore
+* */
 public class test {
 }
