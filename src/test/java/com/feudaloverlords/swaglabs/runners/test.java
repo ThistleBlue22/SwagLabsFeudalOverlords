222809b48e3ca14a314358ec2cd0c807487cfe12
andymurray32 <andrewmurray5@outlook.com>
2022-03-03 10:13:38 +0000

    TestRunner template
    just a basic template to be changed with the creation of feature files

--- src/test/java/com/feudaloverlords/swaglabs/runners/test.java
+++ src/test/java/com/feudaloverlords/swaglabs/runners/test.java
@@ -1,4 +1,13 @@
 package com.feudaloverlords.swaglabs.runners;
 
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+//@RunWith( Class running with)
+@CucumberOptions(
+    //features={"  insert features path   "}
+        //features={"pretty","html:target/testReport.html","json:target/jsonReport.json"}
+        //publish= true;
+        )
 public class test {
 }
