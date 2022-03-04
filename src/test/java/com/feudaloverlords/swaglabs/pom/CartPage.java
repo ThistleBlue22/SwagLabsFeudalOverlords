6a5685ac1fa219020a6aa5d6b2e0803f733b98f9
Autumn Pelešová <autumnlara@hotmail.com>
2022-03-04 11:50:02 +0000

    Implemented several methods requested by stepdef team

--- src/test/java/com/feudaloverlords/swaglabs/pom/CartPage.java
+++ src/test/java/com/feudaloverlords/swaglabs/pom/CartPage.java
@@ -19,10 +19,22 @@ public class CartPage extends Page {
                 driver.findElement(BY_CHECKOUT).click();
                 return new CheckoutOnePage(driver);
             }
-        };
+        }
+    }
 
-        private static final By BY_CONTINUE_SHOPPING = new By.ById("continue-shopping"),
-                                BY_CHECKOUT = new By.ById("checkout");
+    private static final By
+            BY_CONTINUE_SHOPPING = new By.ById("continue-shopping"),
+            BY_CHECKOUT = new By.ById("checkout"),
+            BY_INVENTORY_ITEM_NAME = new By.ByClassName("inventory_item_name");
+
+    /**
+     * Checks if the given item is in the cart.
+     * @param i The item to check the presence of.
+     * @return true if the item is in the cart, otherwise false.
+     */
+    public boolean isItemInCart(Item i) {
+        return driver.findElements(BY_INVENTORY_ITEM_NAME).stream()
+                .anyMatch(w -> w.getText().equals(i.NAME));
     }
 
     public CartPage(WebDriver driver) {
--- src/test/java/com/feudaloverlords/swaglabs/pom/InventoryPage.java
+++ src/test/java/com/feudaloverlords/swaglabs/pom/InventoryPage.java
@@ -1,18 +1,38 @@
 package com.feudaloverlords.swaglabs.pom;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 
 public class InventoryPage extends Page{
 
+    private static final By BY_IMG = new By.ByClassName("inventory_item_img");
+    private static final String IMG_SOURCE_DOG = "/static/media/sl-404.168b1cce.jpg";
+
     public InventoryPage(WebDriver driver) {
         super(driver, "https://www.saucedemo.com/inventory.html");
     }
 
+    /**
+     * Adds the given item to the cart.
+     * @param i the item to add.
+     */
     public void addItemToCart(Item i) {
-        driver.findElement(i.BY).click();
+        driver.findElement(i.BY_ADD).click();
     }
 
+    /**
+     * Removes the given item from the cart.
+     * @param i the item to remove.
+     */
+    public void removeItem(Item i) {
+        driver.findElement(i.BY_REMOVE).click();
+    }
+
+    /**
+     * Retrieves the number on the cart badge.
+     * @return the number on the cart badge, or 0 if there isn't one.
+     */
     public int getCartBadgeNumber() {
         int cartBadgeNumber = 0;
         try {
@@ -21,5 +41,14 @@ public class InventoryPage extends Page{
         } finally {
             return cartBadgeNumber;
         }
+    }
+
+    /**
+     * Checks if all item images are the dog.
+     * @return true if all images are the dog, otherwise false.
+     */
+    public boolean areAllImagesTheDog() {
+        return driver.findElements(BY_IMG).stream()
+                .allMatch(w -> w.getAttribute("src").equals(IMG_SOURCE_DOG));
     }
 }
