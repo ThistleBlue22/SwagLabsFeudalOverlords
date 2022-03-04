fe68aaed20599086ae0a3af6fff516462d44a4f3
Gideon9876543 <98388179+Gideon9876543@users.noreply.github.com>
2022-03-04 11:42:38 +0000

    InventoryItem Page Updated
    POM class for each individual item has been updated.

---
+++ src/test/java/com/feudaloverlords/swaglabs/pom/InventoryItemPage.java
@@ -0,0 +1,40 @@
+package com.feudaloverlords.swaglabs.pom;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class InventoryItemPage extends Page{
+
+    public enum Links implements LinksInterface {
+        BACK_TO_PRODUCTS {
+            @Override
+            public Page getPage(WebDriver driver) {
+                driver.findElement(BY_BACK_TO_PRODUCTS).click();
+                return new InventoryPage(driver);
+            }
+        },
+        ADD_TO_CART {
+            @Override
+            public Page getPage(WebDriver driver) {
+                driver.findElement(BY_BACK_TO_PRODUCTS).click();
+                return new InventoryItemPage(driver);
+            }
+        },
+        REMOVE {
+            @Override
+            public Page getPage(WebDriver driver) {
+                driver.findElement(BY_REMOVE).click();
+                return new InventoryItemPage(driver);
+            }
+        };
+        private static final By BY_BACK_TO_PRODUCTS = new By.ByXPath("//*[@id=\"back-to-products\"]"),
+                BY_ADD_TO_CART = new By.ByXPath("///*[@id=\"add-to-cart-sauce-labs-bike-light\"]"),
+                BY_REMOVE = new By.ByXPath("//*[@id=\"remove-sauce-labs-bike-light\"]");
+    }
+
+
+
+    public InventoryItemPage(WebDriver driver) {
+        super(driver, "https://www.saucedemo.com/inventory-item.html?id=");
+    }
+}
